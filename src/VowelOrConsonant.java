5d08ffec6d725b2260b4b3d9f6409d4ac53dbd00
zinia3131 <ziniaasraf1234@gmail.com>
2024-11-22 09:48:12 +0600

    E-5.20_Finding Vowel Or Consonant Program has been commited

---
+++ src/VowelOrConsonant.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class VowelOrConsonant {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a single character: ");
+        String input = scanner.next();
+
+        if (input.length() != 1 || !Character.isLetter(input.charAt(0))) {
+            System.out.println("Error: Invalid input.");
+        } else {
+            char ch = Character.toLowerCase(input.charAt(0));
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                System.out.println("Vowel");
+            } else {
+                System.out.println("Consonant");
+            }
+        }
+    }
+}
+
